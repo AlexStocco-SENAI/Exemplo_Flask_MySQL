18fb22de3d6eb854306060365f035994023cf659
alexstocco <alexstocco.senai@gmail.com>
2025-10-21 10:15:04 -0300

    Deploy

--- conexao.py
+++ conexao.py
@@ -2,12 +2,22 @@ import mysql.connector
 
 class Conexao:
     
+    # def conectar():
+    #     #Conectando ao banco de dados
+    #     mydb = mysql.connector.connect(
+    #         host="127.0.0.1",
+    #         user="usuario_exemplo",
+    #         password="senha_exemplo",
+    #         database="bd_exemplo"
+    #         )
+    #     return mydb
+
     def conectar():
         #Conectando ao banco de dados
         mydb = mysql.connector.connect(
-            host="127.0.0.1",
-            user="usuario_exemplo",
-            password="senha_exemplo",
+            host="banco-de-dados-godofredo.mysql.database.azure.com",
+            user="godoadmin",
+            password="Alexlindao@123",
             database="bd_exemplo"
             )
         return mydb
